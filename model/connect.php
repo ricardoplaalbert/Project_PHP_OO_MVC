82c8b7a08ff72b6345cc36e16a05e6fc31c46c94
Ricardo Pla <58602396+ricardoplaalbert@users.noreply.github.com>
2020-12-10 21:37:24 +0100

    Update connect.php

--- model/connect.php
+++ model/connect.php
@@ -3,7 +3,7 @@
 		public static function con(){
 			$host = 'localhost';  
     		$user = "root";                     
-    		$pass = "chilenas3";                             
+    		$pass = "";                             
     		$db = "Reservas";                      
 
     		$conexion = mysqli_connect($host, $user, $pass, $db);
